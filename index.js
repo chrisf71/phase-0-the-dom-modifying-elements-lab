4ea016ba326fcd56ac3e49fae980fd8b81cc7a3d
chrisf71 <christopherf442@hotmail.com>
2023-12-31 23:14:04 +0000

    completed dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.querySelector('#main');
+if (mainElement) {
+  mainElement.remove();
+}
+const victoryHeader = document.querySelector('h1#victory');
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+newHeader.textContent = "Christopher is the champion;" 
